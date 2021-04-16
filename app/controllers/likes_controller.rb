1771e5d90aef53507ab6d7e4c61767b8aba598e2
Nordin Belkacemi <belkaceminordin@gmail.com>
2021-04-16 02:38:04 +0200

    like link works with ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,28 +1,34 @@
 class LikesController < ApplicationController
-    before_action :set_likeable, only: [:new]
+    before_action :set_likeable, only: [:create]
 
     def create
-        likeable = nil
-        if params[:post_id].present?
-            likeable = Post.find(params[:post_id])
-        end
-
-        if params[:comment_id].present?
-            likeable = Comment.find(params[:comment_id]) 
-        end
-
-        @like = Like.new(likeable: likeable, user: current_user)
+        @like = Like.new(likeable: @likeable, user: current_user)
         if @like.save
-            redirect_to posts_path
-        else
-            redirect_to posts_path
+            respond_to do |format|
+                format.js
+            end
         end
     end
 
     def destroy
-        
+        @like = Like.find(params[:id])
+        @like.destroy
+
+        respond_to do |format|
+            format.js
+        end
     end
 
     private
+
+    def set_likeable
+        if params[:post_id].present?
+            @likeable = Post.find(params[:post_id])
+        end
+
+        if params[:comment_id].present?
+            @likeable = Comment.find(params[:comment_id]) 
+        end
+    end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,10 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
+        # Post.all.each do |post|
+        #     liked = Like.where(likeable: post, user: current_user).any?
+        #     posts << {post: post, liked: liked}
+        # end
     end
 
     def edit
@@ -22,5 +26,4 @@ class PostsController < ApplicationController
     def destroy
 
     end
-
 end
