a4a568908436bd0fb0868392fdfaec01ff0e1fb5
Nordin Belkacemi <belkaceminordin@gmail.com>
2021-05-13 13:08:06 +0200

    cleaned up the views a little (likes for posts and comments) + added some helpers

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,13 @@
 module LikesHelper
+    def is_like_count_on_left(like)
+        like.likeable_type == "Comment"
+    end
+
+    def new_like_path(likeable)
+        if likeable.class == Comment
+            comment_likes_path(likeable)
+        elsif likeable.class == Post
+            post_likes_path(likeable)
+        end
+    end
 end
